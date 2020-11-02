fb90e9c2cb0ab1bc58172a1419ec4f4f7577d6ab
Andrew Medford <ajm@gatech.edu>
2020-11-02 18:18:12 -0500

    updated gamma beta comparison

--- gamma_beta_comparison.py
+++ gamma_beta_comparison.py
@@ -2,6 +2,7 @@ import pylab as plt
 from SIR import *
 from scipy.stats import rv_histogram
 import numpy as np
+import matplotlib.gridspec as gridspec
 
 R0 = 2.5
 
@@ -15,9 +16,23 @@ BetaModel = SymmetricBetaSIR(ab=1,
                  epmax=2.,
                  n_bins=50*1000)
 
-def dynamics_plot(model, param_name, param_list, solution_axes, dynamics_axes, order_axes, n_timeslices=12, markers=['o', 's', '^', '+', '*'], linestyles = ['-', '--', ':', '-.', '-']):
+fig = plt.figure(constrained_layout=True, figsize=(8, 6))
+gs = fig.add_gridspec(3, 6)
 
-    for ax, d_ax, K_ax, param, mark, ls in zip(solution_axes, dynamics_axes, order_axes, param_list, markers, linestyles):
+ax_gamma_results = fig.add_subplot(gs[:2, :3])
+
+ax_gamma_1 = fig.add_subplot(gs[2, 0])
+ax_gamma_2 = fig.add_subplot(gs[2, 1])
+ax_gamma_3 = fig.add_subplot(gs[2, 2])
+
+ax_beta_results = fig.add_subplot(gs[:2, 3:])
+ax_beta_1 = fig.add_subplot(gs[2, 3])
+ax_beta_2 = fig.add_subplot(gs[2, 4])
+ax_beta_3 = fig.add_subplot(gs[2, 5])
+
+def dynamics_plot(model, param_name, param_list, solution_axes, dynamics_axes,  n_timeslices=12, markers=['o', 's', '^', '+', '*'], linestyles = ['-', '--', ':', '-.', '-']):
+
+    for ax, d_ax, param, mark, ls in zip(solution_axes, dynamics_axes, param_list, markers, linestyles):
 
         setattr(model, param_name, param)
         model.x_list = []
@@ -27,22 +42,17 @@ def dynamics_plot(model, param_name, param_list, solution_axes, dynamics_axes, o
 
         tbin, xSbin, xIbin, xRbin, xImaxbin = model.get_result(mode='binnedSIR')
 
-        ax.plot(tbin, xRbin, label='binnedSIR({}:{} = {:.2f})'.format(*model.get_param_info()) + '-{} bins'.format(model.n_bins), ls=ls, color='k', alpha=0.5)
-        ax.plot(tbin[::spacing], xRbin[::spacing], label='binnedSIR({}:{} = {:.2f})'.format(*model.get_param_info()) + '-{} bins'.format(model.n_bins), ls='none', color='k', alpha=0.5, marker=mark, markersize=4)
+        if param_name == 'ab':
+            pname = 'a = b'
+        else:
+            pname = param_name
 
-        dSdt = np.gradient(xSbin, tbin)
-        logdSdt = np.log(-dSdt)
-        logxS = np.log(xSbin)
-
-        K = np.gradient(logdSdt, logxS)
-        mask = np.abs(dSdt) > 1e-3
-        print(sum(mask))
-        print(logdSdt.shape)
-
-        K_ax.plot(tbin[mask], K[mask], label='binnedSIR({}:{} = {:.2f})'.format(*model.get_param_info()) + '-{} bins'.format(model.n_bins), ls=ls, color='k', alpha=0.5)
+        ax.plot(tbin, xRbin, label="{} = {}".format(pname, param), ls=ls, color='k', alpha=0.5)
 
         ax.set_xlabel('Time [arbitrary]')
         ax.set_ylabel('Proportion Infected')
+
+        ax.legend()
 
         step_size = int(len(model.x_list)/n_timeslices)
 
@@ -55,26 +65,32 @@ def dynamics_plot(model, param_name, param_list, solution_axes, dynamics_axes, o
 
         for t_i, hist_i, color in zip(times, distros, colors):
             # each entry of the x_list contains all bin info for xS ([:-2]), xI ([:-2]), and xR ([:-1]).
-            #axes[0].plot([t_i], [hist_i[-1]], marker='|', markersize=10, color=color)
             if d_ax:
+                ax.plot([t_i], [hist_i[-1]], marker='|', markersize=10, color=color)
+
+                #d_ax.annotate('{} = {}'.format(pname, param), [0.5, 0.7], xycoords='axes fraction')
 
                 d_eps = model.epmax/model.n_bins
                 hist = hist_i[:-2]/d_eps
-                d_ax.plot(bin_means, hist, color=color, alpha=0.5, ls='-', lw=0.5, label="t = {:.1f}".format(t_i))
+                d_ax.plot(bin_means, hist, color=color, alpha=0.5, ls='-', lw=1.5, label="t = {:.1f}".format(t_i))
 
                 d_ax.set_xlim([0, min(model.epmax, 4)])
                 d_ax.set_ylim([0, 1.1])
-                d_ax.set_xlabel('Susceptibility [unitless]')
-                d_ax.set_ylabel('Probability')
 
-fig1, axes_1 = plt.subplots(2, 2, figsize=(8,7))
 
-fig2, axes_2 = plt.subplots(2, 3, figsize=(8,9))
+dynamics_plot(GammaModel, 'k', [0.5, 1, 2], solution_axes = [ax_gamma_results]*3, dynamics_axes = [ax_gamma_1, ax_gamma_2, ax_gamma_3])
 
-dynamics_plot(GammaModel, 'k', [0.5, 1, 2], solution_axes = [axes_1[0,0]]*3, dynamics_axes = axes_2[0,:], order_axes=[axes_1[0,1]]*3)
+dynamics_plot(BetaModel, 'ab', [0.5, 1, 2], solution_axes = [ax_beta_results]*3, dynamics_axes = [ax_beta_1, ax_beta_2, ax_beta_3])
 
-dynamics_plot(BetaModel, 'ab', [0.5, 1, 2], solution_axes = [axes_1[1,0]]*3, dynamics_axes = axes_2[1,:], order_axes=[axes_1[1,1]]*3)
+for ax in [ax_gamma_results, ax_beta_results]:
+    ax.set_ylim([0, 0.8])
 
-axes_2[0,0].legend()
+#ax_gamma_2.set_xlabel('Susceptibility [unitless]')
+#ax_gamma_1.set_ylabel('Probability')
+
+#ax_beta_2.set_xlabel('Susceptibility [unitless]')
+#ax_beta_1.set_ylabel('Probability')
+
+fig.savefig('gamma_beta_comparison.pdf')
 
 plt.show()
